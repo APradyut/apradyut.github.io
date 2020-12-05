047a993ae8277d34739da784910348c12ad8e45d
apradyut <adarsh.pradyut@gmail.com>
2020-12-06 02:16:58 +0530

    Resume download option and experience status indicator

---
+++ common.js
@@ -0,0 +1,31 @@
+function documentOnload() {
+  verifyCSSGrid();
+}
+function verifyCSSGrid() {
+  // create a div (could also reuse a known element)
+  var el = document.createElement("div");
+  // detect support
+  var supports_grid = typeof el.style.grid === "string";
+  // add the output to the bucket
+  if (!supports_grid) {
+    alert("This browser is out dated and might not open the website properly");
+  }
+}
+function downloadResume() {
+  var resumeURL =
+    "https://github.com/APradyut/apradyut.github.io/raw/master/Resume%20NET.pdf";
+  download(resumeURL);
+}
+
+function download(url) {
+  var element = document.createElement("a");
+  element.setAttribute("href", url);
+  element.setAttribute("download", "Resume - Adarsh Pradyut");
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
